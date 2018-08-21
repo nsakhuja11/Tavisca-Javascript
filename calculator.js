1b1a5a754ac4041ec9cd53c5dd9782e98e8187d4
Nihit Sakhuja <nsakhuja@tavisca.com>
2018-08-21 15:12:43 +0530

    History Functionality added

--- calculator.js
+++ calculator.js
@@ -1,10 +1,13 @@
 var flag=0;
-
+var hist="<br>";
 function clr(v) {
 	document.getElementById("display").value=v;
 	flag=0;
 }
-
+function displayhistory()
+{
+	document.getElementById("hist-display").innerHTML=hist;
+}
 function val(v) {
 	if(v=="*" || v=="/" || v=="-" || v=="+"){
 		flag=0;
@@ -20,18 +23,26 @@ function val(v) {
 }
 
 function evl() { 
-	try 
-	{ 
-	  clr(eval(document.getElementById("display").value))
-	  flag=1;
+	var exp=document.getElementById("display").value;
+	try { 
+		var result=eval(exp);
+		clr(result);
+		hist=hist+exp+" = "+result+"<br><br>";
+		flag=1;
 	} 
-	catch(e) 
-	{
-	  clr('Error')
+	catch(e) {
+		clr('Error');
+		flag=1;
 	} 
 } 
 
 function enterKey(e) {
+	if(e.key=="/" || e.key=="*" || e.key=="-" || e.key=="+"){
+		flag=0;
+	}
+	if(flag==1) {
+		clr("");
+	}
 	if(e.keyCode==13)
 	{
 		evl();
